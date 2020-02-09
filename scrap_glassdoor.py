85ee982afaa84026d3614803bc7a62fb5538bf4d
Weichi Zhao <wzhao42@asu.edu>
2020-02-08 23:49:02 -0700

    Adding new files

---
+++ scrap_glassdoor.py
@@ -0,0 +1,10 @@
+import time
+import json
+import Review
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
